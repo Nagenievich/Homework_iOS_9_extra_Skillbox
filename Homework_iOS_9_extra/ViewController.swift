b26731025d18ec83a54c4118e8e3bb241b04d886
Alexey Makarov <alexeynkrv@gmail.com>
2021-06-10 13:08:35 +0700

    Initial Commit

---
+++ Homework_iOS_9_extra/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_iOS_9_extra
+//
+//  Created by Alexey Makarov on 10.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
